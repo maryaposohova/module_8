f0469786a3cd5148c18d11099abebd82143e4f52
maryaposohova <maryaposohova1202@gmail.com>
2025-01-04 00:44:08 +0500

    Add files via upload

--- module_8_3_hw_creatу_except_car.py
+++ module_8_3_hw_creatу_except_car.py
@@ -1,3 +1,67 @@
+'''1'''
+
+# class IncorrectVinNumber(Exception):
+#     def __init__(self, message):
+#         self.message = message
+#
+#     def __str__(self):
+#         print("Ошибка ввода вин номера")
+#
+#
+# class IncorrectCarNumbers(Exception):
+#     def __init__(self, message):
+#         self.message = message
+#
+#     def __str__(self):
+#         print("Ошибка ввода номера авто")
+#
+# class Car:
+#     def __init__(self, model, vin, numbers):
+#         self.model = model
+#         if not isinstance(vin, int):
+#             raise IncorrectVinNumber('Некорректный тип vin номерa')
+#         if not 1000000 <= vin <= 9999999:
+#             raise IncorrectVinNumber('Неверный диапазон для vin номера')
+#         else:
+#             self.vin = vin
+#         if not isinstance(numbers, str):
+#             raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+#         if len(numbers) != 6:
+#             raise IncorrectCarNumbers('Неверная длина номера')
+#         else:
+#             self.numbers = numbers
+#
+#
+# try:
+#   first = Car('Model1', 1000000, 'f123dj')
+# except IncorrectVinNumber as exc:
+#   print(exc.message)
+# except IncorrectCarNumbers as exc:
+#   print(exc.message)
+# else:
+#   print(f'{first.model} успешно создан')
+#
+#
+# try:
+#   second = Car('Model2', 300, 'т001тр')
+# except IncorrectVinNumber as exc:
+#   print(exc.message)
+# except IncorrectCarNumbers as exc:
+#   print(exc.message)
+# else:
+#   print(f'{second.model} успешно создан')
+#
+# try:
+#   third = Car('Model3', 2020202, 'нет номера')
+# except IncorrectVinNumber as exc:
+#   print(exc.message)
+# except IncorrectCarNumbers as exc:
+#   print(exc.message)
+# else:
+#   print(f'{third.model} успешно создан')
+
+
+'''2'''
 class IncorrectVinNumber(Exception):
     def __init__(self, message):
         self.message = message
@@ -30,7 +94,6 @@ class Car:
         if len(number) != 6:
             raise IncorrectCarNumbers('Неверная длина номера')
         return True
-
 
 
 try:
